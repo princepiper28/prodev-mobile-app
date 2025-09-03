b817268150bc7474660b49040c48c41c168a2597
prince joseph udoewah <princepiper28@gmail.com>
2025-09-04 00:19:22 +0100

    Add: join screen styles in styles/_join.ts

---
+++ prodev-mobile-app-0x04/styles/_join.ts
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+const joinStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "700",
+    color: "#333",
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#666",
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#2B876E",
+    padding: 15,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+});
+
+export { joinStyles };
